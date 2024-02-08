9b438201c431bf109303d63c0cd59d782dac540f
TWKTemplar <carlegos101@gmail.com>
2024-02-07 22:50:08 -0600

    Got frame by frame playback working
    Optimized randamization table
    Added adjustable frames
    Made fade effect last between frames
    Added save frame print

--- RenderBadAppleGraph.py
+++ RenderBadAppleGraph.py
@@ -5,41 +5,43 @@ import os
 import random
 import math
 import time
-def Render(imgpath):
-    # Get Data
-    img = Image.open(imgpath)
-    appleImageData = list(img.getdata())
-    # Make Image
-    graph = np.zeros((img.height,img.width))
-    for x in range(img.height):  # up and down
-        for y in range(img.width):#left n right
-            graph[x, y] = GetPixel(x, y, appleImageData, img.width, graph[x, y])
-
-    SaveImage(graph)
+def Render(numberOfImagesToRender):
+    if(numberOfImagesToRender <=0):
+        numberOfImagesToRender = 1
+    count = 0
+    appleimg = Image.open(GetInputImagePath(count))
+    graph = np.zeros((appleimg.height, appleimg.width))
+    for i in range(numberOfImagesToRender):
+        appleimg = Image.open(GetInputImagePath(count))
+        appleImageData = list(appleimg.getdata())
+        for x in range(appleimg.height):
+            for y in range(appleimg.width):
+                graph[x, y] = GetPixel(x, y, appleImageData, appleimg.width, graph[x, y])
+        SaveImage(graph, count)
+        count += 1
 def GetPixel(x, y, pixel_data, imgWidth, currentPixel):
     appleImageValueRGB = pixel_data[x * imgWidth + y]
     targetVal = int(((sum(appleImageValueRGB) / 3 ) > 128))
     if (currentPixel == targetVal):
         return currentPixel
-    ra = random.randint(0,1)
-    rb = random.randint(0, 1)
-    rc = random.randint(0, 1)
-    rd = random.randint(0, 1)
+    ra = random.randint(0,5)
     if (currentPixel == 0):
-        return (ra*rb*rc*rd)
+        return int(ra==0)
     if (currentPixel == 1):
-        return (ra*rb*rc*rd)
-def SaveImage(grid):
+        return int(ra==0)
+def SaveImage(grid, imageID):
     plt.imshow(grid, vmin=0, vmax=1)  # Assuming binary grid, using grayscale colormap
     #plt.axis('off')  # Turn off axis
     if not os.path.exists("Render"):
         os.makedirs("Render")
-    plt.savefig("Render/apple.png", dpi=300, bbox_inches='tight', pad_inches=0)  # Save the image
-def GetSourceImagePath():
+    name = "Render/A"+str(imageID)+".png"
+    plt.savefig(name, dpi=50, bbox_inches='tight', pad_inches=0)  # Save the image
+    print("Saved: " + name)
+def GetInputImagePath(number):
     if not os.path.exists("BadApple"):
         os.makedirs("BadApple")
     imageFolder = "BadApple"
-    imageName = "86"
+    imageName = str(number)
     imagePath = os.path.join(imageFolder, imageName + ".png")
     return imagePath
-Render(GetSourceImagePath())
+Render(499)
